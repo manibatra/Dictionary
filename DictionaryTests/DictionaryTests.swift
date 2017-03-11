dc74681cef35243fc325f821839f9ce056247507
Mani Batra <manibatra2002@gmail.com>
2017-03-11 14:37:09 +1100

    Initial Commit

---
+++ DictionaryTests/DictionaryTests.swift
@@ -0,0 +1,36 @@
+//
+//  DictionaryTests.swift
+//  DictionaryTests
+//
+//  Created by Mani Batra on 11/3/17.
+//  Copyright © 2017 Mani Batra. All rights reserved.
+//
+
+import XCTest
+@testable import Dictionary
+
+class DictionaryTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
